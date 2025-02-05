f764f3f3841ab8b759d3d1959d1fda864d0c1a3d
ilana <ilanaaprilloriram@gmail.com>
2025-02-05 19:28:26 +0200

    add minor debug print

--- tests/test_data_base.py
+++ tests/test_data_base.py
@@ -113,17 +113,20 @@ class TestDataBase:
             print(f"File: {i}{file_record['source_file']}, date: {file_record['date']}")
             db_member.insert(f"{i}{file_record['source_file']}",                                     
                                 file_record["date"])
-        # Get all files from a table
+        # Get all files from a table - we get already modeled items
         files = db_member.get_all_files()
         assert len(files) == 3
         
         for file in files:
-            # this way we convert model into struct and can get to each field of the stract. Here model is a dict. Each memver has 2 fileds:
-            # source_file and date 
+            # way1: files already modeled - so we can print them directly
+            print(f"File: {file.source_file}, date: {file.date}")
+
+            # way2: this way we convert model into struct and can get to each field of the stract. Here model is a dict. Each memver has 2 fileds:
+            # source_file and date             
             file_obj = my_file_model(**file.model_dump())
-            print(f"File: {file_obj.source_file}, date: {file_obj.date}")  
-         
-        
+            print(f"File: {file_obj.source_file}, date: {file_obj.date}")           
+                
+
         # all will return True only if all iterations will be resulted with True, assert will be issued if all will not return True
         assert all(isinstance(file, my_file_model)
                     for file in files)

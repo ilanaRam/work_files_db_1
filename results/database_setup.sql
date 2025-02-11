67ceebac13fff13d8fa62c44fae35a8050bae936
ilana <ilanaaprilloriram@gmail.com>
2025-02-11 11:17:03 +0200

    added results directory with sql file

--- results/database_setup.sql
+++ results/database_setup.sql
@@ -4,8 +4,8 @@
                                         source_file TEXT NOT NULL,
                                         date TEXT NOT NULL);
                                         
-INSERT INTO files_store (source_file, date) VALUES ('addPCHTierBeforeSiteShield_20201017-223034.txt', '2025-02-10 16:17:23');
-INSERT INTO files_store (source_file, date) VALUES ('respprovider_20201017-1000.txt', '2025-02-10 16:17:23');
-INSERT INTO files_store (source_file, date) VALUES ('respprovider_20201017-223534.txt', '2025-02-10 16:17:23');
-INSERT INTO files_store (source_file, date) VALUES ('test_1.txt', '2025-02-10 16:17:23');
-INSERT INTO files_store (source_file, date) VALUES ('test_2.txt', '2025-02-10 16:17:23');
+INSERT INTO files_store (source_file, date) VALUES ('addPCHTierBeforeSiteShield_20201017-223034.txt', '2025-02-11 08:48:29');
+INSERT INTO files_store (source_file, date) VALUES ('respprovider_20201017-1000.txt', '2025-02-11 08:48:29');
+INSERT INTO files_store (source_file, date) VALUES ('respprovider_20201017-223534.txt', '2025-02-11 08:48:29');
+INSERT INTO files_store (source_file, date) VALUES ('test_1.txt', '2025-02-11 08:48:29');
+INSERT INTO files_store (source_file, date) VALUES ('test_2.txt', '2025-02-11 08:48:29');
